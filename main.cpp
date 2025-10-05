ec9a439d789492396e076f65b26a29512c355d22
BondOleksandr <hgess1488@gmail.com>
2025-10-05 12:39:59 +0000

    task 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double* pMatrix; // First argument - initial matrix
+double* pVector; // Second argument - initial vector
+double* pResult; // Result vector for matrix-vector multiplication
+int Size; // Sizes of initial matrix and vector
+
+int main() {
+    cout << "Serial matrix-vector multiplication program" << endl;
+    cin.get();  // щоб почекати натискання клавіші (аналог getch)
+    return 0;
+}
